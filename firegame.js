3d9070e56fa880a658802d4f17995803dca33071
tyson daugherty <tyson.daugherty@gmail.com>
2014-05-30 11:58:10 -0700

    tic tac toe working

--- firegame.js
+++ firegame.js
@@ -5,25 +5,45 @@ var myDataStore = new Firebase('https://missionfire.firebaseio.com/');
 
 var sessionTurn = new Firebase('https://missionfire.firebaseio.com/turn');
 
+var sessionElem = new Firebase('https://missionfire.firebaseio.com/elem');
+
 var turn = 0;
+
+// var addElem = null; 
 
 var renderTurn = function (turn) {
 	console.log(turn);
-	if (turn > 0) {
-		if (turn % 2 == 0) {
-		console.log('i am even');
-		} else {
-			console.log('i am odd');
-		}
-	}	
-
 	// $('.score-value').remove();
 	// $('.score-total').append($('<span class="score-value">' + score + '</span>'));
 }
 
+var renderElem = function (elem) {
+	console.log(elem);
+	console.log(turn);
+	$('#' + elem).remove();
+  var newElId = ('#' + elem + '-div');
+	console.log(newElId);
+	if (turn > 0) {
+		if (turn % 2 == 0) {
+			$(newElId).append($('<span class="turn-value even"> O </span>'))
+		} else {
+			$(newElId).append($('<span class="turn-value odd"> X </span>'))
+		}
+	}	
+}
+
+
+sessionElem.on('value', function(snapshot) {
+  var addElem = snapshot.val();
+  console.log(addElem);
+	renderElem(addElem);
+});
+
+
+
 sessionTurn.on('value', function(snapshot) {
   turn = snapshot.val();
-	renderTurn(turn);
+	// renderTurn(turn);
 });
 
 $('.not-played').click(function(e) {
@@ -32,21 +52,28 @@ $('.not-played').click(function(e) {
 	console.log(elId);
 	var newTurn = turn + 1;
 	myDataStore.update({turn: newTurn});
-	$(this).remove();
+
+	myDataStore.update({elem: elId});
+	// $(this).remove();
 	console.log(newTurn);
-	if (newTurn > 0) {
-		if (newTurn % 2 == 0) {
-			$('#' + elId + '-div').append($('<span class="turn-value even"> O </span>'))
-		} else {
-			console.log('i am odd');
-			$('#' + elId + '-div').append($('<span class="turn-value odd"> X </span>'))
-		}
-	}	
+	// if (newTurn > 0) {
+	// 	if (newTurn % 2 == 0) {
+	// 		addElem = ('#' + elId + '-div');
+	// 		console.log(addElem);
+	// 		// $('#' + elId + '-div').append($('<span class="turn-value even"> O </span>'))
+	// 	} else {
+	// 		console.log('i am odd');
+	// 		addElem = ('#' + elId + '-div');
+	// 		console.log(addElem);
+	// 		// $('#' + elId + '-div').append($('<span class="turn-value odd"> X </span>'))
+	// 	}
+	// }	
 })
 
 $('.new-game').click(function(e) {
 	e.preventDefault();
 	myDataStore.update({turn: 0});
+	myDataStore.update({elem: null});
 	console.log('new game set');
 })
 
