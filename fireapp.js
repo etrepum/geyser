e5fe8b09fba22e3c5d77dcb4a7d66a8c0b23f054
Bob Ippolito <bob@redivi.com>
2014-05-30 13:31:20 -0700

    handle initial score

--- fireapp.js
+++ fireapp.js
@@ -5,18 +5,19 @@
 
 var sessionScore = new Firebase('https://missionfire.firebaseio.com/score');
 
-var oldVal;
+var oldVal = 0;
 
 var renderScore = function (score) {
-	console.log(score);
-	$('.score-value').text(score);
+    console.log(score);
+    $('.score-value').text(score);
 };
 
 sessionScore.on('value', function(snapshot) {
-    oldVal = snapshot.val();
-	renderScore(oldVal);
+    // snapshot.val() may be null the first time, Number(null) is 0
+    oldVal = Number(snapshot.val());
+    renderScore(oldVal);
 });
 
 $('#score-button').on('click', function(e) {
-	sessionScore.set(oldVal + 5);
+    sessionScore.set(oldVal + 5);
 });
